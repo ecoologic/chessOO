4db59161c698fd5658b73c3d9eacc0d67a4cad67
Erik Ecoologic <erikecoologic@gmail.com>
2019-03-30 23:51:37 +1000

    Refactor into Position::Delta (p1)

--- app/position.rb
+++ app/position.rb
@@ -1,6 +1,56 @@
 # Wraps the info 'A1' to provides helper methods
 # like coordinates [x, y], distances etc
 class Position
+  class Delta
+    def initialize(position_a, position_b)
+      @position_a, @position_b = position_a, position_b
+    end
+
+    # TODO: all_along_diagonal
+    def all_between_diagonal
+      x_range = Range.new(*[position_a.x, position_b.x].sort)
+      y_range = Range.new(*[position_a.y, position_b.y].sort)
+      zipped = x_range.zip(y_range)
+
+      result = zipped.map { |x, y| Position.from_coordinates([x, y]) }
+      result[1..result.length - 2] # Exclude start and destination
+    end
+
+    # TODO: +1 below...
+    def all_between_along_axes
+      if position_a.letter == position_b.letter
+        # Loop numbers
+        y_range = Range.new(*[position_a.y, position_b.y].sort)
+        result = y_range.map { |path_y| Position.new("#{position_a.letter}#{path_y + 1}") }
+        result[1..result.length - 2] # Exclude start and destination
+      else
+        # Loop letters
+        x_range = Range.new(*[position_a.x, position_b.x].sort)
+        # TODO:
+        [Position.new('E1')]
+      end
+    end
+
+    def all_between
+      if delta.x.zero? || delta.y.zero?
+        all_between_along_axes
+      else
+        all_between_diagonal
+      end
+    end
+
+    def delta
+      delta_x = position_b.x - position_a.x
+      delta_y = position_b.y - position_a.y
+
+      Position.from_coordinates([delta_x, delta_y])
+    end
+
+    private
+
+    attr_reader :position_a, :position_b
+  end
+
   def self.from_coordinates(coordinate_couple)
     x, y = coordinate_couple
     letter_x = ('A'.bytes.first + x).chr
@@ -33,22 +83,6 @@ class Position
     [x, y]
   end
 
-  # TODO: Position::Delta ??
-  def all_between(destination_position)
-    if delta(destination_position).x.zero? || delta(destination_position).y.zero?
-      all_between_along_axes(destination_position)
-    else
-      all_between_diagonal(destination_position)
-    end
-  end
-
-  def delta(destination_position)
-    delta_x = destination_position.x - x
-    delta_y = destination_position.y - y
-
-    self.class.from_coordinates([delta_x, delta_y])
-  end
-
   def letter
     ('A'.bytes.first + x).chr
   end
@@ -57,31 +91,24 @@ class Position
     y + 1
   end
 
-  private
-
-  attr_reader :value
-
-  # TODO: all_along_diagonal
+  # TODO: delete
   def all_between_diagonal(destination_position)
-    x_range = Range.new(*[destination_position.x, x].sort)
-    y_range = Range.new(*[destination_position.y, y].sort)
-    zipped = x_range.zip(y_range)
-
-    result = zipped.map { |x, y| self.class.from_coordinates([x, y]) }
-    result[1..result.length - 2] # Exclude start and destination
+    Delta.new(self, destination_position).all_between_diagonal
   end
 
   def all_between_along_axes(destination_position)
-    if letter == destination_position.letter
-      # Loop numbers
-      y_range = Range.new(*[destination_position.y, y].sort)
-      result = y_range.map { |path_y| self.class.new("#{letter}#{path_y + 1}") }
-      result[1..result.length - 2] # Exclude start and destination
-    else
-      # Loop letters
-      x_range = Range.new(*[destination_position.x, x].sort)
-      # TODO:
-      [self.class.new('E1')]
-    end
+    Delta.new(self, destination_position).all_between_along_axes
   end
+
+  def all_between(destination_position)
+    Delta.new(self, destination_position).all_between
+  end
+
+  def delta(destination_position)
+    Delta.new(self, destination_position).delta
+  end
+
+  private
+
+  attr_reader :value
 end
